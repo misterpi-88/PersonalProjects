d60414ee4f48a78b175ca74ef422f1e59ff4936d
misterpi-88 <288614741+misterpi-88@users.noreply.github.com>
2026-09-11 23:15:10 +0200

    Unit tests created for Get method

--- ToDo/ApiTests/ToDoTaskEndpointsTests.cs
+++ ToDo/ApiTests/ToDoTaskEndpointsTests.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
+using ToDo.Api.Endpoints;
+using ToDo.Application.Common.Dto;
 using ToDo.Application.Services;
 
 namespace ApiTests;
@@ -20,10 +23,67 @@ public class ToDoTaskEndpointsTests
     }
 
     [Fact]
-    public void Example()
+    public async Task Get_WhenCalled_CallsService()
     {
-        var act = true;
+        var id = 1;
 
-        Assert.True(act);
+        var act = await ToDoTaskEndpoints.Get(id, _toDoTaskServiceMock.Object, _loggerFactory, _httpContext);
+
+        _toDoTaskServiceMock.Verify(x => x.Get(id), Times.Once);
+    }
+
+    [Fact]
+    public async Task Get_HasNoToDoTask_ReturnsNotFound()
+    {
+        var act = await ToDoTaskEndpoints.Get(1, _toDoTaskServiceMock.Object, _loggerFactory, _httpContext);
+
+        Assert.IsType<NotFound>(act.Result);
+    }
+
+    [Fact]
+    public async Task Get_HasToDoTask_ReturnsOk()
+    {
+        var id = 1;
+
+        _toDoTaskServiceMock.Setup(x=> x.Get(id)).ReturnsAsync(new GetToDoTaskDto
+        {
+            Id = id,
+            Task = "Write some unit tests",
+            Created = DateTime.UtcNow
+        });
+
+        var act = await ToDoTaskEndpoints.Get(id, _toDoTaskServiceMock.Object, _loggerFactory, _httpContext);
+
+        Assert.IsType<Ok<GetToDoTaskDto>>(act.Result);
+    }
+
+    [Fact]
+    public async Task Get_HasToDoTask_ReturnsExpectedInstance()
+    {
+        var id = 1;
+        var expectedResult = new GetToDoTaskDto
+        {
+            Id = id,
+            Task = "Write some unit tests",
+            Created = DateTime.UtcNow
+        } ;
+
+        _toDoTaskServiceMock.Setup(x=> x.Get(id)).ReturnsAsync(expectedResult);
+
+        var act = await ToDoTaskEndpoints.Get(id, _toDoTaskServiceMock.Object, _loggerFactory, _httpContext);
+
+        Assert.Equal(expectedResult,((Ok<GetToDoTaskDto>)act.Result).Value);
+    }
+
+    [Fact]
+    public async Task Get_ThrowsException_ReturnsInternalServerError()
+    {
+        var id = 1;
+
+        _toDoTaskServiceMock.Setup(x=> x.Get(id)).ThrowsAsync(new Exception("Broken"));
+
+        var act = await ToDoTaskEndpoints.Get(id, _toDoTaskServiceMock.Object, _loggerFactory, _httpContext);
+
+        Assert.IsType<InternalServerError>(act.Result);
     }
 }
