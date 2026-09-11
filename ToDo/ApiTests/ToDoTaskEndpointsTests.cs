d0436f41d35e345090298d909cb4a1d74e5f4951
misterpi-88 <288614741+misterpi-88@users.noreply.github.com>
2026-09-11 21:43:29 +0200

    Created xUnit project, mocked a service and wrote an Example test

---
+++ ToDo/ApiTests/ToDoTaskEndpointsTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using ToDo.Application.Services;
+
+namespace ApiTests;
+
+public class ToDoTaskEndpointsTests
+{
+    private readonly Mock<IToDoTaskService> _toDoTaskServiceMock;
+    private readonly HttpContext _httpContext;
+    private readonly ILoggerFactory _loggerFactory;
+
+    public ToDoTaskEndpointsTests()
+    {
+        _toDoTaskServiceMock = new Mock<IToDoTaskService>();
+        _httpContext = new DefaultHttpContext();
+        _loggerFactory = new NullLoggerFactory();
+    }
+
+    [Fact]
+    public void Example()
+    {
+        var act = true;
+
+        Assert.True(act);
+    }
+}
